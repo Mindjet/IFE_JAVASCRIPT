c988bff9d0435a51d2715d8542eb658376d35f63
Mindjet <578746875@qq.com>
2016-04-21 13:17:11 +0800

    add timer

--- task22/script.js
+++ task22/script.js
@@ -1,25 +1,31 @@
 
 
 var colorLib = ['tomato','orange','black','green'];
-
+var renderList = [];
+var timer = null;
 
 function handleBtn(){
-	
+
 	var DF = document.getElementById('traversalDF');
 	DF.onclick = function(){
+		myReset();
 		traversalDF(callback);
+		render();
 	}
 
 	var BF = document.getElementById('traversalBF');
 	BF.onclick = function(){
+		myReset();
 		traversalBF(callback);
+		render();
 	}
 
 	// callback can change backcolor in both types of traversal
 	callback = function(node){
 		for (var i = 0; i < node.length; i++) {
 			console.log(node[i].className.substring(6));
-			node[i].style.backgroundColor = colorLib[Math.ceil(Math.random()*4)-1];
+			// node[i].style.backgroundColor = colorLib[Math.ceil(Math.random()*4)-1];
+			renderList.push(node);
 		};
 	}
 
@@ -39,7 +45,7 @@ function traversalDF(callback){
 				};
 			};
 		};
-		setTimeout(callback(node),2000);
+		callback(node);
 
 	})('child-0');
 
@@ -63,6 +69,32 @@ function traversalBF(callback){
 		currentNode = queue.dequeue();
 
 	}
+
+}
+
+function render(){
+	var tiktok = 0;
+	console.log(renderList[0]);
+	// renderList[tiktok].style.backgroundColor = 'blue';
+	// timer = setInterval(function(){
+	// 	tiktok++;
+	// 	if (tiktok<renderList.length) {
+	// 		renderList[tiktok-1].style.backgroundColor = 'white';
+	// 		renderList[tiktok].style.backgroundColor = 'blue';
+	// 	}else if (tiktok == renderList.length) {
+	// 		clearInterval(timer);
+	// 		renderList[tiktok-1].style.backgroundColor = 'white';
+	// 	};
+
+	// },500);
+}
+
+function myReset(){
+	
+	clearInterval(timer);
+	for (var i = 0; i < renderList.length; i++) {
+		renderList[i].style.backgroundColor = 'white';
+	};
 
 }
 
