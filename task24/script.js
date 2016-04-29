54c0b0eeb81c6025f39fa37ae150529214c073ed
Mindjet <578746875@qq.com>
2016-04-29 18:48:32 +0800

    finish task24

--- task24/script.js
+++ task24/script.js
@@ -3,22 +3,121 @@
 var renderList = [];
 var timer = null;
 
+/*-------------------handle the popup menu event---------------*/
+function popupEvent(){
+
+	var tree = getByClass('tree', null)[0];
+	var divList = tree.getElementsByTagName('div');
+	var spanList = tree.getElementsByTagName('span');
+	var popup = getByClass('popup', null)[0];
+	var btn_del = document.getElementById('delete');
+
+
+	document.onclick = function(){
+		myReset();
+		popup.style.display = 'none';
+	}
+
+	// initialize the click-show event
+	for (var i = 0; i < divList.length; i++) {
+
+		divList[i].onmouseover = function(event){
+			this.style.cursor = 'pointer';
+			event.stopPropagation();
+		}
+
+		divList[i].onclick = click_show_process;
+	};
+}
+
+// handle the divs' click-show-process event
+function click_show_process(event){
+
+	myReset();
+
+	var popup = getByClass('popup', null)[0];
+	var btn_del = document.getElementById('delete');
+	var btn_add = document.getElementById('add');
+	var input = document.getElementById('add-item');
+	input.focus();
+	var object;
+
+	popup.style.display = 'inline-block';
+
+	
+	// judge whether the DIV or SPAN is clicked
+	if (event.target.tagName == 'DIV') {
+		object = event.target;
+	}else if (event.target.tagName == 'SPAN') {
+		object = event.target.parentNode;
+	};
+
+	object.style.backgroundColor = 'tomato';
+
+	// postion the popup menu
+	mleft = event.clientX;
+	mtop = event.clientY;
+	popup.style.left = mleft+'px';
+	popup.style.top = mtop+'px';
+
+	input.onclick = function(e){
+		e.stopPropagation();
+	}
+
+	btn_del.onclick = function(e){
+		object.remove();
+		popup.style.display = 'none';
+		e.stopPropagation();
+	}
+
+	btn_add.onclick = function(e){
+
+		var name = input.value.trim();
+		
+		if (name!='') {
+			var newItem = document.createElement('div');
+			newItem.className = name;
+			var span = document.createElement('span');
+			span.innerHTML = name.substring(0, 1).toUpperCase()+name.substring(1);
+			newItem.appendChild(span);
+			newItem.style.backgroundColor = 'white';
+			object.appendChild(newItem);
+			input.value = '';
+			popup.style.display = 'none';
+		}else{
+			alert('please enter something!');
+		}
+		
+		e.stopPropagation();
+	}
+
+	event.stopPropagation();
+	popupEvent();
+}
+
+/*---------------------------------------------------------------------*/
+
+/*---------------------handle the search event-------------------------*/
 function handlerSearch(){
 	
 	var search_DF = document.getElementById('traversalDF_search');
 	var search_BF = document.getElementById('traversalBF_search');
+	var popup = getByClass('popup', null)[0];
 
 	search_DF.onclick = function(){
-
 		myReset();
 		traversalDF(callback);
 		render();
+		popup.style.display = 'none';
+		event.stopPropagation();
 	}
 
 	search_BF.onclick = function(){
 		myReset();
 		traversalBF(callback);
 		render();
+		popup.style.display = 'none';
+		event.stopPropagation();
 	}
 
 	var callback = function(node){
@@ -72,6 +171,7 @@ function traversalBF(callback){
 
 }
 
+
 function render(){
 
 	var exist = false;
@@ -111,11 +211,14 @@ function render(){
 
 }
 
+/*---------------------------------------------------------------------*/
+
 function myReset(){
 	
 	clearInterval(timer);
 	renderList = [];
-	var divList = document.getElementsByTagName('div');
+	var tree = getByClass('tree',null)[0];
+	var divList = tree.getElementsByTagName('div');
 	for (var i = 0; i < divList.length; i++) {
 		divList[i].style.backgroundColor = 'white';
 		divList[i].className = divList[i].className.replace('-MARKED','');
@@ -160,8 +263,8 @@ function dequeue(){
 /*-----------------------------------------------------*/
 
 
-window.onload=function(){
+window.onload = function(){
+	myReset();
 	handlerSearch();
-	var result = getByClass('result',null)[0];
-	result.style.display = 'none';
+	popupEvent();
 }
